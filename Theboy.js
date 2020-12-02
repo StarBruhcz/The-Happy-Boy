4ce3b22dae49925877c651ad8b461b69c218fa1c
StarBruh <69429226+StarBruhcz@users.noreply.github.com>
2020-12-02 21:24:49 +0100

    1

--- Theboy.js
+++ Theboy.js
@@ -4,27 +4,29 @@ const prefix = '*';
 
 client.on('ready', () =>{
     console.log("READY");
-})
-
-client.on('message', message => {
-    if (message.content == "TEST") {
-        message.channel.send("TEST STATUS: SUCCED!");
-        console.log("TEST SUCCESFUL");
-    }
-})
+});
 
 client.on('message', async message => {
-    if (message.content == "restart") {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+	const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const cmd = args.shift().toLowerCase();
+    
+    if(cmd == 'meme')
+    {
+        const subreddits = ["dankmeme", "meme", "me_irl"];
+        const random = subreddits[Math.floor(Math.random() * subreddits.length)];
 
-        if (!message.author.id == '725072066050654249') {
-            return message.reply("You cant use this command! Only the developer of this bot can do it.");
-        }
+        const img = await randomPuppy(random);
+        const MEME= new Discord.MessageEmbed()
+            .setColor("RANDOM")
+            .setImage(img)
+            .setTitle(`from /r/${random}`)
+            .setURL(`https://reddit.com/r/${random}`);
 
-        await message.channel.send("RESTARTING....")
-        process.exit();
+            message.channel.send("Finding some **fresh** memes..");
+            await message.channel.send(MEME);
     }
-})
+});
 
-//client.login(process.env.TOKEN);
-client.login('Nzc3NTE1MDY3MDYzMTQwMzYy.X7EjVQ.sF6LB4cs8mPR3lx8cEZwHmmPg8I');
+client.login(process.env.token);
